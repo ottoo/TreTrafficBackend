e23b18e30cd3e2a00bfcd52b47ece2799c25493d
Otto Kivikärki <otto.kivikarki@vincit.fi>
2018-09-13 12:11:31 +0300

    Add route for fetching available bus lines

--- server/common.js
+++ server/common.js
@@ -1,5 +1,11 @@
 const processVehicleData = payload => (payload.body ? payload.body.map(val => val.monitoredVehicleJourney) : []);
 
+const processLines = payload => payload.body ? payload.body.map(({ name, description }) => ({
+  name,
+  description
+})) : [];
+
 module.exports = {
-  processVehicleData
+  processVehicleData,
+  processLines
 };
--- server/routes.js
+++ server/routes.js
@@ -1,7 +1,10 @@
 const Wreck = require('wreck');
-const { processVehicleData } = require('./common.js');
+const { processVehicleData, processLines } = require('./common.js');
 
-const API_URL = 'http://data.itsfactory.fi/journeys/api/1/vehicle-activity';
+const VEHICLE_ACTIVITY_API_URL = 'http://data.itsfactory.fi/journeys/api/1/vehicle-activity';
+const LINES_API_URL = 'http://data.itsfactory.fi/journeys/api/1/lines';
+
+const readResponse = async response => Wreck.read(response, { json: true });
 
 module.exports = [
   {
@@ -13,14 +16,21 @@ module.exports = [
   },
   {
     method: 'GET',
-    path: '/api/lines',
+    path: '/api/vehicle-activity',
     handler: async request => {
-      const lines = request.query.lineRefs;
-      const readResponse = async response => processVehicleData(await Wreck.read(response, { json: true }));
-      const apiUrl = lines ? `${API_URL}?lineRef=${lines}` : API_URL;
+      const lines = request.query.lineRef;
+      const apiUrl = lines ? `${VEHICLE_ACTIVITY_API_URL}?lineRef=${lines}` : VEHICLE_ACTIVITY_API_URL;
 
       const response = await Wreck.request('GET', apiUrl);
-      return readResponse(response);
+      return processVehicleData(await readResponse(response));
+    }
+  },
+  {
+    method: 'GET',
+    path: '/api/lines',
+    handler: async () => {
+      const response = await Wreck.request('GET', LINES_API_URL);
+      return processLines(await readResponse(response));
     }
   }
 ];
